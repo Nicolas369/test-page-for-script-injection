725ebf7d75dbdcf6c21aff5e995e914e83bcef51
Nicolas369 <nicolas.gabrenas@gmail.com>
2024-01-11 22:07:31 -0300

    imports ethers.js [count:3]

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function testScriptImportedFromGithub(){
 }
 
 async function importEthersModule() {
-    const ethers = fetch("https://cdn.ethers.io/lib/ethers-5.2.esm.min.js", {
+    const ethers = await fetch("https://cdn.ethers.io/lib/ethers-5.2.esm.min.js", {
         method: "GET",
         mode: "no-cors",
     });
