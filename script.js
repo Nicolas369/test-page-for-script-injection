5b23a8b8697bc2615ebc3f9a1c84b395f7ac12b9
Nicolas369 <nicolas.gabrenas@gmail.com>
2024-01-11 21:56:34 -0300

    adding ethers.js import [count:1]

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function testScriptImportedFromGithub(){
     alert("script working!!!");
 }
+
+async function importEthersModule() {
+    const ethers = await import("https://cdn.ethers.io/lib/ethers-5.2.esm.min.js");
+    return ethers
+}
